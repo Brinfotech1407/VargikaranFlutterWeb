903fd9a5112cba757f8342e2fc2157360dce488e
Ramu Pal <ramu.pal2603@gmail.com>
2023-04-19 18:42:54 +0530

    Added Firebase Configuration. And SetState issue Resolved.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:vargikaran_web_app/login_screen.dart';
 import 'package:vargikaran_web_app/starter/home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: 'AIzaSyCqakKc0Ge18dZTrhDH8SXRoLm94SqWtyc',
+          appId: '1:924078074109:web:b01fcf505359f4385f7694',
+          messagingSenderId: '924078074109',
+          projectId: 'vargikarannvs'));
   runApp(const MyApp());
 }
 
@@ -20,29 +27,29 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
- // Locale? _locale;
-  ValueNotifier<Locale> _locale = ValueNotifier(Locale('en'));
+  Locale? _locale = const Locale('en');
+
+  // ValueNotifier<Locale> _locale = ValueNotifier(Locale('en'));
 
   setLocale(Locale locale) {
-      _locale.value = locale;
+    _locale = locale;
+    if (mounted) {
+      setState(() {});
+    }
   }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder<Locale>(
-      valueListenable: _locale,
-      builder: (context, value, child) {
-        return MaterialApp(
-          title: 'VargiKaran',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          locale: value,
-          localizationsDelegates: AppLocalizations.localizationsDelegates,
-          supportedLocales: AppLocalizations.supportedLocales,
-          home: HomePage(),
-        );
-      },
+    return MaterialApp(
+      title: 'VargiKaran',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      locale: _locale,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: HomePage(),
     );
   }
 }
